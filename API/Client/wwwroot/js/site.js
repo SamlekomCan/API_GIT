b1e2a01ec08d59ee7e42112056c72e7a04f1d04b
Ican <icanikhsan44@gmail.com>
2022-02-04 16:41:08 +0700

    asd

--- API/Client/wwwroot/js/site.js
+++ API/Client/wwwroot/js/site.js
@@ -2,3 +2,27 @@
 // for details on configuring this project to bundle and minify static web assets.
 
 // Write your JavaScript code.
+let animals = [
+    { name: "dory", species: "fish", class: { name: "invertebrata" } },
+    { name: "simba", species: "cat", class: { name: "mamalia" } },
+    { name: "tori", species: "cat", class: { name: "mamalia" } },
+    { name: "nemo", species: "fish", class: { name: "invertebrata" } },
+    { name: "budi", species: "cat", class: { name: "mamalia" } }
+]
+
+for (var i = 0; i < animals.length; i++) {
+    if (animals[i].species == "fish") {
+        animals[i].class.name = "Non-Mamalia"
+    }
+}
+console.log("ANIMALS")
+console.log(animals)
+
+let OnlyCat = []
+for (var i = 0; i < animals.length; i++) {
+    if (animals[i].species == "cat") {
+        OnlyCat.push(animals[i])
+    }
+}
+console.log("ONLYCATS")
+console.log(OnlyCat)
